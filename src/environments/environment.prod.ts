1115d08db2415d410d4297a5d133c12a47f6abe4
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-06-26 11:56:01 +0100

    firebase moved to env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzW88qIYi_YC7PqqO7tSMnlGmPar0U4d0",
+    authDomain: "chat-7a4d5.firebaseapp.com",
+    databaseURL: "https://chat-7a4d5.firebaseio.com",
+    projectId: "chat-7a4d5",
+    storageBucket: "chat-7a4d5.appspot.com",
+    messagingSenderId: "55126876398"
+  }
 };
